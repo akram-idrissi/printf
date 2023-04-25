8f07ce2009df50eaed13985323c6086f65f02972
Mochatr <mohamedchatr8@gmail.com>
2023-04-25 01:35:22 -0700

    Fixing betty

--- get_flags.c
+++ get_flags.c
@@ -13,25 +13,29 @@ int get_flags(const char *format, int *i)
 	/* 1 2 4 8  16 */
 	int j, curr_i;
 	int flags = 0;
-	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'}; //These are the characters that represents the different flags
-	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0}; //These are the flag values (bitwise OR of F_MINUS, F_ZERO,F_HASH, and F_SPACE)
+	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
+	/*These are the characters that represents the different flags*/
+	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
+	/*flag values (bitwise OR of F_MINUS, F_ZERO,F_HASH, and F_SPACE)*/
 
-	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++) //Iterate the characters in 'format' starting from the character after the '%'
+	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+	/*Iterate the char in 'format' starting from the char after the '%'*/
 	{
-		for (j = 0; FLAGS_CH[j] != '\0'; j++) //Check if the current character matches any of the flag characters
+		for (j = 0; FLAGS_CH[j] != '\0'; j++)
+	/*Check if the current char matches any of the flag characters*/
 			if (format[curr_i] == FLAGS_CH[j])
 			{
-			//If it does, set the corresponding bit in the 'flags' integer using bitwise OR
+	/*If it does, set the corresponding bit in the 'flags' int using bitwise OR*/
 				flags |= FLAGS_ARR[j];
 				break;
 			}
-		//If we reach a character that is not a flag character, stop parsing
+	/*If we reach a char that is not a flag character, stop parsing*/
 
 		if (FLAGS_CH[j] == 0)
 			break;
 	}
 
-	// Update the value of i so that it points to the last flag character that was parsed
+	/*Update the value of 'i'*/
 	*i = curr_i - 1;
 
 	return (flags);
--- get_width.c
+++ get_width.c
@@ -12,30 +12,29 @@ int get_width(const char *format, int *i, va_list list)
 {
 	int curr_i;
 	int width = 0;
-	// Iterate over the characters in 'format' starting from the character after the '%'
+	/*Iterate over the chars in 'format' starting from the char after the '%'*/
 	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
 	{
-		// if the current character is a digit, add it to 'width' 
+		/*if the current character is a digit, add it to 'width'*/
 		if (is_digit(format[curr_i]))
 		{
 			width *= 10;
 			width += format[curr_i] - '0';
 		}
-		// if the current character is a '*', read the next argument from 'list' as width
+		/*if the current character is a '*', read the argument from 'list' as width*/
 		else if (format[curr_i] == '*')
 		{
 			curr_i++;
 			width = va_arg(list, int);
 			break;
 		}
-		// if the current character is not a digit or '*', stop parsing
+		/* if the current character is not a digit or '*', stop parsing*/
 		else
 			break;
 	}
-	// Update the value of 'i' so that it points to the last width character that was parsed
+	/*Update the value of 'i'*/
 
 	*i = curr_i - 1;
-	// Return the width of the field to be printed
 
 	return (width);
 }
